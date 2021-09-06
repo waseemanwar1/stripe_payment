b99a3636d6aeb21bcb96cca19fcc2bd1eb6757f4
Ammad Fareed <ammad.fareed786@gmail.com>
2021-09-06 12:46:27 +0500

    Implemented payment feature for stripe

--- lib/stripe_payment.rb
+++ lib/stripe_payment.rb
@@ -3,6 +3,36 @@
 require_relative "stripe_payment/version"
 
 module StripePayment
-  class Error < StandardError; end
-  # Your code goes here...
+  class Payment
+    require 'stripe'
+
+    def initialize(stripe_secret_key)
+      Stripe.api_key = stripe_secret_key
+    end
+
+    def create(params)
+      token = create_token(params[:card_number], params[:card_exp_month], params[:card_exp_year], params[:card_cvc])
+      charge = create_charge(params[:amount], params[:currency], params[:description], token.id)
+    end
+
+    def create_token(card_number, card_exp_month, card_exp_year, card_cvc)
+      Stripe::Token.create({
+        card: {
+          number: card_number,
+          exp_month: card_exp_month,
+          exp_year: card_exp_year,
+          cvc: card_cvc
+        }
+      })
+    end
+
+    def create_charge(amount, currency, description, token_id)
+      Stripe::Charge.create({
+        amount: amount * 100,
+        currency: currency,
+        description: description,
+        source: token_id
+      })
+    end
+  end
 end
